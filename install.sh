0c7dc1d17716d43a916793858e470bd76a4a5787
Jose Pedro Pires Joao <ze.pedro.joao@gmail.com>
2025-07-19 17:32:39 +0100

    Instalar Flatpak

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Update package lists
+# Update package list
 sudo apt update
+
+# Install Flatpak and GNOME plugin (optional but recommended)
+sudo apt install -y flatpak gnome-software-plugin-flatpak
+
+# Add Flathub repository if not already added
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 
 # List of applications to install
 apps=(
